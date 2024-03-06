8de496ae29702dcc6cb08a735e55823ffdf5cfb2
Avishek Behera <avishekbehera87@gmail.com>
2024-03-06 14:57:25 +0530

    (feat): Initial commit with base framework files

---
+++ README.md
@@ -0,0 +1,2 @@
+### Exploring Rest Assured
+This project is to learn and explore Rest Assured in depth.
---
+++ src/main/java/com/techiewolf/config/properties/ConfigurationFactory.java
@@ -0,0 +1,22 @@
+package com.techiewolf.config.properties;
+
+import org.aeonbits.owner.ConfigCache;
+import org.aeonbits.owner.ConfigFactory;
+
+public final class ConfigurationFactory {
+
+  /**
+   * This class is restricted to be extended or to be instantiated, Leaving only option to use the
+   * static methods via class name
+   */
+  private ConfigurationFactory() {}
+
+  public static EnvironmentConfig getEnvironmentConfig() {
+    ConfigFactory.setProperty("environment", getFrameworkConfig().environment());
+    return ConfigCache.getOrCreate(EnvironmentConfig.class);
+  }
+
+  public static FrameworkConfig getFrameworkConfig() {
+    return ConfigCache.getOrCreate(FrameworkConfig.class);
+  }
+}
